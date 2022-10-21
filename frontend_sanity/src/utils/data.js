0ec68d5666a0c1ae0324bc682db422ae154240ed
Ismael148 <zolalainaismael@gmail.com>
2022-10-21 22:56:06 +0200

    First Commit

---
+++ frontend_sanity/src/utils/data.js
@@ -0,0 +1,300 @@
+export const categories = [
+  {
+    name: 'Current events',
+    image:
+      'https://images.unsplash.com/photo-1664716022220-99b92000a5eb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDh8QkpKTXR0ZURKQTR8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=60',
+  },
+  {
+    name: 'Food & Drink',
+    image:
+      'https://images.unsplash.com/photo-1665762984428-5b92b5852c84?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDN8eGpQUjRobGtCR0F8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=60',
+  },
+  {
+    name: 'Wallpapers',
+    image:
+      'https://i.pinimg.com/236x/03/48/b6/0348b65919fcbe1e4f559dc4feb0ee13.jpg',
+  },
+  {
+    name: '3D Renders',
+    image:
+      'https://images.unsplash.com/photo-1666010896273-978032e6ba2d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDN8Q0R3dXdYSkFiRXd8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=60',
+  },
+  {
+    name: 'Textures & Patterns',
+    image:
+      'https://images.unsplash.com/photo-1665963287479-105466e03324?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDF8aVVJc25WdGpCMFl8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=60',
+  },
+  {
+    name: 'Experimental',
+    image:
+      'https://images.unsplash.com/photo-1661805262969-42ed67348982?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDR8cVBZc0R6dkpPWWN8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=60',
+  },
+  {
+    name: 'Architecture',
+    image:
+      'https://images.unsplash.com/photo-1665714644892-1ae70e986235?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDF8cm5TS0RId3dZVWt8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=60',
+  },
+  {
+    name: 'Nature',
+    image:
+      'https://i.pinimg.com/236x/b9/82/d4/b982d49a1edd984c4faef745fd1f8479.jpg',
+  },
+  {
+    name: 'Business & Work',
+    image:
+      'https://images.unsplash.com/photo-1624355511224-14da0f11e256?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDZ8YWV1NnJMLWo2ZXd8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=60',
+  },
+  {
+    name: 'Fashion',
+    image:
+      'https://images.unsplash.com/photo-1665504953294-495725f8113a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDJ8UzRNS0xBc0JCNzR8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=60',
+  },
+  {
+    name: 'Film',
+    image:
+      'https://images.unsplash.com/photo-1665923809161-a01963e43c12?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDF8aG1lbnZRaFVteE18fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=60',
+  },
+  {
+    name: 'Health & Wellness',
+    image:
+      'https://images.unsplash.com/photo-1551076805-e1869033e561?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDZ8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+  },
+  {
+    name: 'People',
+    image:
+      'https://images.unsplash.com/photo-1665504281546-993297b2ff51?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDF8dG93SlpGc2twR2d8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=60',
+  },
+  {
+    name: 'Interiors',
+    image:
+      'https://images.unsplash.com/photo-1665761125784-e432f7f8539e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDF8Ul9GeW4tR3d0bHd8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=60',
+  },
+  {
+    name: 'Street Photography',
+    image:
+      'https://images.unsplash.com/photo-1666103117159-2ba19151502d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDF8eEh4WVRNSExnT2N8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=60',
+  },
+  {
+    name: 'Spirituality',
+    image:
+      'https://images.unsplash.com/photo-1666209925785-d9facf88a438?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDN8Xzh6Rkh1aFJoeW98fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=60',
+  },
+  {
+    name: 'Animals',
+    image:
+      'https://images.unsplash.com/photo-1665508425173-53b9deb8a44a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDF8SnBnNktpZGwtSGt8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=60',
+  },
+  {
+    name: 'Cars',
+    image:
+      'https://i.pinimg.com/750x/eb/47/44/eb4744eaa3b3ccd89749fa3470e2b0de.jpg',
+  },
+  {
+    name: 'Fitness',
+    image:
+      'https://i.pinimg.com/236x/25/14/29/251429345940a47490cc3d47dfe0a8eb.jpg',
+  },
+
+  {
+    name: 'Websites',
+    image:
+      'https://i.pinimg.com/750x/66/b1/29/66b1296d36598122e6a4c5452b5a7149.jpg',
+  },
+  {
+    name: 'Art & Cultures',
+    image:
+      'https://i.pinimg.com/736x/f4/e5/ba/f4e5ba22311039662dd253be33bf5f0e.jpg',
+  },
+  {
+    name: 'Travel',
+    image:
+      'https://i.pinimg.com/236x/fa/95/98/fa95986f2c408098531ca7cc78aee3a4.jpg',
+  },
+  {
+    name: 'Quotes',
+    image:
+      'https://i.pinimg.com/236x/46/7c/17/467c17277badb00b638f8ec4da89a358.jpg',
+  },
+  {
+    name: 'History',
+    image:
+      'https://images.unsplash.com/photo-1665739825278-5205b5fd3b90?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDJ8ZGlqcGJ3OTlrUVF8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=60',
+  },
+  {
+    name: 'Athletic',
+    image:
+      'https://images.unsplash.com/photo-1665953735748-8b296f4e6292?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDF8Qm4tRGpyY0Jyd298fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=60',
+  },
+  {
+    name: 'Others',
+    image:
+      'https://i.pinimg.com/236x/2e/63/c8/2e63c82dfd49aca8dccf9de3f57e8588.jpg',
+  },
+];
+
+export const feedQuery = `*[_type == "pin"] | order(_createdAt desc) {
+  image{
+    asset->{
+      url
+    }
+  },
+      _id,
+      destination,
+      postedBy->{
+        _id,
+        userName,
+        image
+      },
+      save[]{
+        _key,
+        postedBy->{
+          _id,
+          userName,
+          image
+        },
+      },
+    } `;
+
+export const pinDetailQuery = (pinId) => {
+  const query = `*[_type == "pin" && _id == '${pinId}']{
+    image{
+      asset->{
+        url
+      }
+    },
+    _id,
+    title, 
+    about,
+    category,
+    destination,
+    postedBy->{
+      _id,
+      userName,
+      image
+    },
+   save[]{
+      postedBy->{
+        _id,
+        userName,
+        image
+      },
+    },
+    comments[]{
+      comment,
+      _key,
+      postedBy->{
+        _id,
+        userName,
+        image
+      },
+    }
+  }`;
+  return query;
+};
+
+export const pinDetailMorePinQuery = (pin) => {
+  const query = `*[_type == "pin" && category == '${pin.category}' && _id != '${pin._id}' ]{
+    image{
+      asset->{
+        url
+      }
+    },
+    _id,
+    destination,
+    postedBy->{
+      _id,
+      userName,
+      image
+    },
+    save[]{
+      _key,
+      postedBy->{
+        _id,
+        userName,
+        image
+      },
+    },
+  }`;
+  return query;
+};
+
+export const searchQuery = (searchTerm) => {
+  const query = `*[_type == "pin" && title match '${searchTerm}*' || category match '${searchTerm}*' || about match '${searchTerm}*']{
+        image{
+          asset->{
+            url
+          }
+        },
+            _id,
+            destination,
+            postedBy->{
+              _id,
+              userName,
+              image
+            },
+            save[]{
+              _key,
+              postedBy->{
+                _id,
+                userName,
+                image
+              },
+            },
+          }`;
+  return query;
+};
+
+export const userQuery = (userId) => {
+  const query = `*[_type == "user" && _id == '${userId}']`;
+  return query;
+};
+
+export const userCreatedPinsQuery = (userId) => {
+  const query = `*[ _type == 'pin' && userId == '${userId}'] | order(_createdAt desc){
+    image{
+      asset->{
+        url
+      }
+    },
+    _id,
+    destination,
+    postedBy->{
+      _id,
+      userName,
+      image
+    },
+    save[]{
+      postedBy->{
+        _id,
+        userName,
+        image
+      },
+    },
+  }`;
+  return query;
+};
+
+export const userSavedPinsQuery = (userId) => {
+  const query = `*[_type == 'pin' && '${userId}' in save[].userId ] | order(_createdAt desc) {
+    image{
+      asset->{
+        url
+      }
+    },
+    _id,
+    destination,
+    postedBy->{
+      _id,
+      userName,
+      image
+    },
+    save[]{
+      postedBy->{
+        _id,
+        userName,
+        image
+      },
+    },
+  }`;
+  return query;
+};
